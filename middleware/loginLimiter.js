1ba42612a118f02e113ba14443a82a4375aa39d9
ehix <3h1x00@gmail.com>
2023-10-26 18:53:12 +0100

    changes to login limit message

--- middleware/loginLimiter.js
+++ middleware/loginLimiter.js
@@ -5,7 +5,7 @@ const loginLimiter = rateLimit({
     windowMs: 60 * 1000, // 1 minute
     max: 5, // Limit each IP to 5 login requests per `window` per minute
     message:
-        { message: 'Too many login attempts from this IP, please try again after a 60 second pause' },
+        { message: 'Too many login attempts from this IP, please try again after 60 seconds.' },
     handler: (req, res, next, options) => {
         logEvents(`Too Many Requests: ${options.message.message}\t${req.method}\t${req.url}\t${req.headers.origin}`, 'errLog.log')
         res.status(options.statusCode).send(options.message)
